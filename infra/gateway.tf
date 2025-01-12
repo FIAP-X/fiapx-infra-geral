688364c54909fedd03be8a1d69eb7e050fa627fe
DeividCezar <perspicilum@gmail.com>
2025-01-12 13:29:48 -0300

    Update gateway.tf

--- infra/gateway.tf
+++ infra/gateway.tf
@@ -2,19 +2,6 @@ resource "aws_api_gateway_rest_api" "api_gateway" {
   name = "fiapx-gateway"
 }
 
-resource "aws_api_gateway_resource" "lambda_resource" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
-  path_part   = "generate-url"
-}
-
-resource "aws_api_gateway_method" "lambda_method" {
-  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
-  resource_id   = aws_api_gateway_resource.lambda_resource.id
-  http_method   = "POST"
-  authorization = "NONE"
-}
-
 resource "aws_api_gateway_deployment" "api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
   stage_name  = "prod"
