ccc0a514a68e94e9ebb44f655a2d27eeed9bcdaf
DeividCezar <perspicilum@gmail.com>
2025-01-12 13:38:04 -0300

    Update gateway.tf

--- infra/gateway.tf
+++ infra/gateway.tf
@@ -1,8 +1,3 @@
 resource "aws_api_gateway_rest_api" "api_gateway" {
   name = "fiapx-gateway"
 }
-
-resource "aws_api_gateway_deployment" "api_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  stage_name  = "prod"
-}
