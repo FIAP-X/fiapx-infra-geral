43a523c68ddd90f3918619732bbf9669937eb02d
DeividCezar <perspicilum@gmail.com>
2025-01-13 13:11:19 -0300

    Lifecycle para bucket de upload

--- infra/bucket.tf
+++ infra/bucket.tf
@@ -2,6 +2,21 @@ resource "aws_s3_bucket" "bucket_upload" {
   bucket = var.bucket_upload_name
 }
 
+resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
+  bucket = aws_s3_bucket.bucket_upload.id
+
+  rule {
+    id     = "delete-old-objects"
+    status = "Enabled"
+
+    expiration {
+      days = 1
+    }
+
+    filter {}
+  }
+}
+
 resource "aws_s3_bucket" "bucket_processed" {
   bucket = var.bucket_processed_name
 }
