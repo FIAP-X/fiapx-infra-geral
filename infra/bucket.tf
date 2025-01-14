08cfcc7a171c3b09038c6e0c160721c3c12e2c3f
DeividCezar <perspicilum@gmail.com>
2025-01-14 00:58:06 -0300

    Criando bucket notification

--- infra/bucket.tf
+++ infra/bucket.tf
@@ -17,6 +17,20 @@ resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
   }
 }
 
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.bucket_upload.id
+
+  queue {
+    events = ["s3:ObjectCreated:*"]
+    queue_arn = aws_sqs_queue.sqs_processamento.arn
+  }
+
+  depends_on = [
+    aws_sqs_queue.sqs_processamento
+  ]
+}
+
+
 resource "aws_s3_bucket" "bucket_processed" {
   bucket = var.bucket_processed_name
 }
